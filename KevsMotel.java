ea480f4d786cbddfadcbc431a6642712002cde9c
Kev <kjwilkinson1@sky.com>
2018-07-11 10:35:10 +0100

    added new program KevsMotel room numbers with how many guests in each room

---
+++ KevsMotel.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
+import static java.lang.System.out;
+
+class KevsMotel{
+  public static void main(String[] args) throws FileNotFoundException {
+    Scanner diskScanner = new Scanner(new File("guests.txt"));
+
+    out.println("Room\tGuests");
+
+    for (int roomNum = 0; roomNum < 10; roomNum ++){
+      out.print(roomNum);
+      out.print("\t");
+      out.println(diskScanner.nextInt());
+    }
+    diskScanner.close();
+  }
+}
